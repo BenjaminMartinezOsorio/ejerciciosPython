e3d939ff1697f619d80dfb6ec5d83d64722e28a0
Benjamin Martinez <bmo@ciencias.unam.mx>
2023-01-11 12:44:27 -0600

    firs commit

---
+++ listas.py
@@ -0,0 +1,4 @@
+#son ejercicios para practicar listas 
+'''Escribir un programa que almacene las asignaturas de un curso'''
+materias=["español","matematicas","quimica","fisica", "historia" ]
+print (materias)
